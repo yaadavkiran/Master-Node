d70e684080998dfc1172431cf982c93e1a5eab02
kiran <kiran.karigini@microfocus.com>
2020-03-11 19:46:45 +0530

    'test'

--- terraform/deploy.sh
+++ terraform/deploy.sh
@@ -2,8 +2,8 @@
 cd /project/Master-Node/terraform
 sudo terraform init
 sudo terraform apply -auto-approve -lock=false
-sudo touch /tmp/hosts /project/Master-Node/ansible/hosts
-sudo chmod 777 /tmp/hosts /project/Master-Node/ansible/hosts
+sudo touch /tmp/hosts /project/Master-Node/ansible/hosts /project/Master-Node/ansible/output
+sudo chmod 777 /tmp/hosts /project/Master-Node/ansible/hosts /project/Master-Node/ansible/output
 sudo terraform output -json | jq -r '.[keys[0]].value[]','.[keys[1]].value[]' > /tmp/hosts
 sudo awk 'NR==1{print "[Master]"}1 && NR==2{print " "}1 && NR==2{print "[Workers]"}1' /tmp/hosts > /project/Master-Node/ansible/hosts
 sleep 30
