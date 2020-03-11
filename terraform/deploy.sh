d0aa5be09b3904d619c3240352a012a02dcee3a0
kiran <kiran.karigini@microfocus.com>
2020-03-11 15:45:05 +0530

    'test'

--- terraform/deploy.sh
+++ terraform/deploy.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-sudo mkdir -p /ansible
-sudo chmod 777 /ansible
 sudo terraform init
 sudo terraform apply -auto-approve -lock=false
 sudo terraform output -json | jq -r '.[keys[0]].value[]','.[keys[1]].value[]' > /tmp/hosts
-awk 'NR==1{print "[Master]"}1 && NR==2{print " "}1 && NR==2{print "[Workers]"}1' /tmp/hosts > /ansible/hosts
+awk 'NR==1{print "[Master]"}1 && NR==2{print " "}1 && NR==2{print "[Workers]"}1' /tmp/hosts > /project/Master-Node/ansible/hosts
 sleep 30
-cd /ansible
+sudo chmod 777 /project/Master-Node/ansible/
+cd /project/Master-Node/ansible/
 ansible-playbook main.yaml -i hosts
 ansible-playbook k8.yaml -i hosts --limit 'Master'
